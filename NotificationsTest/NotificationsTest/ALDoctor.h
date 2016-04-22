d3d052d1e7477b907fbd456764a7a0601c54e597
Artyom Linnik <artyomlinnik@Artyoms-MacBook-Pro.local>
2016-04-22 21:19:20 +0300

    [+] Added Notitfications

---
+++ NotificationsTest/NotificationsTest/ALDoctor.h
@@ -0,0 +1,16 @@
+//
+//  ALDoctor.h
+//  NotificationsTest
+//
+//  Created by Artyom Linnik on 19/04/16.
+//  Copyright © 2016 Artyom Linnik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ALDoctor : NSObject
+
+@property (assign, nonatomic) float salary;
+
+
+@end
---
+++ NotificationsTest/NotificationsTest/ALDoctor.m
@@ -0,0 +1,13 @@
+//
+//  ALDoctor.m
+//  NotificationsTest
+//
+//  Created by Artyom Linnik on 19/04/16.
+//  Copyright © 2016 Artyom Linnik. All rights reserved.
+//
+
+#import "ALDoctor.h"
+
+@implementation ALDoctor
+
+@end
