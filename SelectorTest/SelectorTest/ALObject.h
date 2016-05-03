85a8de360897fd556df544de4ad384e21a842a90
Artyom Linnik <artyomlinnik@Artyoms-MacBook-Pro.local>
2016-05-03 22:57:59 +0300

    [+] Added SelectorTest

---
+++ SelectorTest/SelectorTest/ALObject.h
@@ -0,0 +1,15 @@
+//
+//  ALObject.h
+//  SelectorTest
+//
+//  Created by Artyom Linnik on 03/05/16.
+//  Copyright © 2016 Artyom Linnik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ALObject : NSObject
+
+- (void) testMethod;
+
+@end
---
+++ SelectorTest/SelectorTest/ALObject.m
@@ -0,0 +1,21 @@
+//
+//  ALObject.m
+//  SelectorTest
+//
+//  Created by Artyom Linnik on 03/05/16.
+//  Copyright © 2016 Artyom Linnik. All rights reserved.
+//
+
+#import "ALObject.h"
+
+@implementation ALObject
+
+- (void) testMethod {
+    NSLog(@"ASObject testMethod");
+}
+
+- (NSString*) superSecretText {
+    return @"I have stolen your candy";
+}
+
+@end
